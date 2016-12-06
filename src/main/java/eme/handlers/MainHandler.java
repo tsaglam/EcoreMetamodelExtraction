ac8cf9079235ecb85b8eb73cf0ecd8d23a5c6ded
Timur Saglam <uleay@student.kit.edu>
2016-12-06 11:09:10 +0100

    INITIAL COMMIT

---
+++ src/main/java/eme/handlers/MainHandler.java
@@ -0,0 +1,20 @@
+package eme.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+/**
+ * Main handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ * @author Timur Saglam
+ */
+public class MainHandler extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        // TODO link to program
+        return null;
+    }
+}
