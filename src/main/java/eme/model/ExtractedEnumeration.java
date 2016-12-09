4b92017e81bc29676c07d5d3661426802dd0a23b
Timur Saglam <uleay@student.kit.edu>
2016-12-09 11:20:00 +0100

    ADD: Added enums and interfaces to the model.

---
+++ src/main/java/eme/model/ExtractedEnumeration.java
@@ -0,0 +1,17 @@
+package eme.model;
+
+/**
+ * Represents an enumeration in the intermediate model.
+ * @author Timur Saglam
+ */
+public class ExtractedEnumeration extends ExtractedType {
+
+    /**
+     * Basic constructor.
+     * @param fullName is the full name, containing name and package name.
+     */
+    public ExtractedEnumeration(String fullName) {
+        super(fullName);
+    }
+
+}
---
+++ src/main/java/eme/model/ExtractedInterface.java
@@ -0,0 +1,17 @@
+package eme.model;
+
+/**
+ * Represents an interface in the intermediate model.
+ * @author Timur Saglam
+ */
+public class ExtractedInterface extends ExtractedType {
+
+    /**
+     * Basic constructor.
+     * @param fullName is the full name, containing name and package name.
+     */
+    public ExtractedInterface(String fullName) {
+        super(fullName);
+    }
+
+}
