e06a55a8f1781501b8a1f77078818f39b7fbcf58
Timur Saglam <uleay@student.kit.edu>
2016-12-09 11:23:53 +0100

    EDIT: Classes in the model can now be abstract.

--- src/main/java/eme/model/ExtractedClass.java
+++ src/main/java/eme/model/ExtractedClass.java
@@ -6,12 +6,22 @@ package eme.model;
  */
 public class ExtractedClass extends ExtractedType {
 
+    private final boolean isAbstract;
+
     /**
      * Basic constructor.
      * @param fullName is the full name, containing name and package name.
      */
-    public ExtractedClass(String fullName) {
+    public ExtractedClass(String fullName, boolean isAbstract) {
         super(fullName);
+        this.isAbstract = isAbstract;
     }
 
+    /**
+     * Checks whether the class is abstract.
+     * @return true if class is abstract.
+     */
+    public boolean isAbstract() {
+        return isAbstract;
+    }
 }
