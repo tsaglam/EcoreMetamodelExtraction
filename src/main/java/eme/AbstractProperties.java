f9a545947a767f59af10ad155444f824866f7767
Timur Saglam <uleay@student.kit.edu>
2016-12-13 13:07:32 +0100

    ADD: Separated the the file management from the properties specific content like default values and getters.

---
+++ src/main/java/eme/AbstractProperties.java
@@ -0,0 +1,98 @@
+package eme;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * The class is the super class for the class ExtractionProperties. It separates the loading, saving
+ * and creating work flow of the properties file from the actual content in the ExtractionProperties
+ * class.
+ * @author Timur Saglam
+ */
+public abstract class AbstractProperties {
+    private static String FILE_COMMENT;
+    private static String FILE_NAME;
+    protected final String path;
+    protected Properties properties;
+
+    /**
+     * Basic constructor. Loads the properties file, creates a new one if the file does not exist.
+     * @param name is the name of the properties file.
+     * @param comment is the comment of the properties file.
+     */
+    public AbstractProperties(String name, String comment) { // TODO (HIGH) fix problem with path
+        FILE_NAME = name;
+        FILE_COMMENT = comment;
+        path = System.getProperty("user.dir") + "/" + FILE_NAME; // create path.
+        if (propertyFileExists()) {
+            load(); // load if file exists.
+        } else {
+            create(); // create if not.
+        }
+    }
+
+    /**
+     * Saves the settings to the properties file.
+     */
+    public void save() {
+        try {
+            FileOutputStream out = new FileOutputStream(path); // create output stream
+            properties.store(out, FILE_COMMENT); // store with stream
+            out.close(); // close stream
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Checks whether the properties file already exists (and is not a folder).
+     * @return true if the file exists.
+     */
+    private boolean propertyFileExists() {
+        File f = new File(path);
+        return f.exists() && !f.isDirectory();
+    }
+
+    /**
+     * creates new properties file.
+     */
+    protected void create() {
+        File file = new File(path); // create file object
+        file.getParentFile().mkdirs(); // be sure that the parent directory exists
+        try {
+            file.createNewFile(); // write file to disk
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        load(); // then load the properties file.
+        setDefaultValues(); // set defaults.
+        save(); // save to file.
+    }
+
+    /**
+     * Loads the settings from the properties file.
+     */
+    protected void load() {
+        try {
+            properties = new Properties(); // create properties object
+            FileInputStream in = new FileInputStream(path); // create input stream
+            properties.load(in); // load from stream
+            in.close(); // close stream
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Sets all the properties to their default values.
+     */
+    abstract protected void setDefaultValues();
+}
--- src/main/java/eme/ExtractionProperties.java
+++ src/main/java/eme/ExtractionProperties.java
@@ -1,36 +1,22 @@
 package eme;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 /**
  * This class manages the extraction properties in the user.properties file.
  * @author Timur Saglam
  */
-public class ExtractionProperties { // TODO (MEDIUM) add setters for properties.
+public class ExtractionProperties extends AbstractProperties { // TODO (MEDIUM) add setters
     private static final String FILE_COMMENT = "Use this file to configure the Ecore metamodel extraction.";
     private static final String FILE_NAME = "user.properties";
+
     public static void main(String[] args) {
         new ExtractionProperties(); // TODO (LOW) remove main method.
     }
-    private final String path;
-
-    private Properties properties;
 
     /**
-     * Basic constructor. Loads the properties file, creates a new one if the file does not exist.
+     * Basic constructor. Calls the super class constructor which manages the file.
      */
     public ExtractionProperties() { // TODO (HIGH) fix problem with path when ran as plugin
-        path = System.getProperty("user.dir") + "/" + FILE_NAME; // create path.
-        if (propertyFileExists()) {
-            load(); // load if file exists.
-        } else {
-            create(); // create if not.
-        }
+        super(FILE_NAME, FILE_COMMENT);
     }
 
     /**
@@ -89,66 +75,8 @@ public class ExtractionProperties { // TODO (MEDIUM) add setters for properties.
         return properties.getProperty("SavingStrategy");
     }
 
-    /**
-     * Saves the settings to the properties file.
-     */
-    public void save() {
-        try {
-            FileOutputStream out = new FileOutputStream(path); // create output stream
-            properties.store(out, FILE_COMMENT); // store with stream
-            out.close(); // close stream
-        } catch (FileNotFoundException exception) {
-            exception.printStackTrace();
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
-    }
-
-    /**
-     * creates new properties file.
-     */
-    private void create() {
-        File file = new File(path); // create file object
-        file.getParentFile().mkdirs(); // be sure that the parent directory exists
-        try {
-            file.createNewFile(); // write file to disk
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
-        load(); // then load the properties file.
-        setDefaultValues(); // set defaults.
-        save(); // save to file.
-    }
-
-    /**
-     * Loads the settings from the properties file.
-     */
-    private void load() {
-        try {
-            properties = new Properties(); // create properties object
-            FileInputStream in = new FileInputStream(path); // create input stream
-            properties.load(in); // load from stream
-            in.close(); // close stream
-        } catch (FileNotFoundException exception) {
-            exception.printStackTrace();
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
-    }
-
-    /**
-     * Checks whether the properties file already exists (and is not a folder).
-     * @return true if the file exists.
-     */
-    private boolean propertyFileExists() {
-        File f = new File(path);
-        return f.exists() && !f.isDirectory();
-    }
-
-    /**
-     * Sets all the properties to their default values.
-     */
-    private void setDefaultValues() {
+    @Override
+    protected void setDefaultValues() {
         properties.setProperty("DefaultPackageName", "DEFAULT");
         properties.setProperty("ExtractEmptyPackages", "true");
         properties.setProperty("ExtractNestedTypes", "true");
