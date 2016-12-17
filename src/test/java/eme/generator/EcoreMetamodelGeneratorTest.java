e6f8b9a7ec6d1a486e038b6a43baf764fcf1e208
Timur Saglam <uleay@student.kit.edu>
2016-12-17 11:47:26 +0100

    EDIT: Removed enumeration test, added null strategy test, renamed tests.

--- src/test/java/eme/generator/EcoreMetamodelGeneratorTest.java
+++ src/test/java/eme/generator/EcoreMetamodelGeneratorTest.java
@@ -1,15 +1,13 @@
 package eme.generator;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
-import org.eclipse.emf.ecore.EEnum;
 import org.eclipse.emf.ecore.EPackage;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import eme.model.ExtractedEnumeration;
+import eme.generator.saving.CustomPathSavingStrategy;
 import eme.model.ExtractedPackage;
 import eme.model.IntermediateModel;
 import eme.properties.ExtractionProperties;
@@ -32,7 +30,7 @@ public class EcoreMetamodelGeneratorTest {
     }
 
     @Test
-    public void testPackages() {
+    public void testPackageStructure() {
         buildMVCPackages();
         EPackage metamodel = generator.generateMetamodelFrom(model);
         assertEquals(properties.getDefaultPackageName(), metamodel.getName());
@@ -43,22 +41,15 @@ public class EcoreMetamodelGeneratorTest {
         assertEquals(3, main.getESubpackages().size());
     }
 
-    @Test
-    public void testEnums() {
-        buildEnum();
-        EPackage metamodel = generator.generateMetamodelFrom(model);
-        assertEquals(1, metamodel.getEClassifiers().size());
-        EEnum enumeration = (EEnum) metamodel.getEClassifiers().get(0);
-        assertEquals("SomeEnum", enumeration.getName());
-        assertEquals(3, enumeration.getELiterals().size());
-        assertNotNull(enumeration.getEEnumLiteral("ONE"));
-        assertNotNull(enumeration.getEEnumLiteral("TWO"));
-        assertNotNull(enumeration.getEEnumLiteral("THREE"));
-    }
-
     @Test(expected = IllegalStateException.class)
     public void testNullSaving() {
         generator.saveMetamodel(); // no metamodel
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullStrategy() {
+        generator.changeSavingStrategy(new CustomPathSavingStrategy()); // has to pass
+        generator.changeSavingStrategy(null); // throws exception
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -74,17 +65,5 @@ public class EcoreMetamodelGeneratorTest {
         model.add(new ExtractedPackage("main.model"));
         model.add(new ExtractedPackage("main.view"));
         model.add(new ExtractedPackage("main.controller"));
-    }
-
-    private void buildEnum() {
-        ExtractedPackage extractedPackage = new ExtractedPackage("");
-        extractedPackage.setAsRoot();
-        ExtractedEnumeration enumeration = new ExtractedEnumeration("SomeEnum");
-        enumeration.addEnumeral("ONE");
-        enumeration.addEnumeral("TWO");
-        enumeration.addEnumeral("THREE");
-        extractedPackage.add(enumeration);
-        model.add(extractedPackage);
-        model.add(enumeration);
     }
 }
