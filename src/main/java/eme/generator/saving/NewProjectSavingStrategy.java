f85a45ce263d84cd88d123606f5dd6b482bee415
Timur Saglam <uleay@student.kit.edu>
2016-12-18 16:29:48 +0100

    EDIT: The saving strategy now uses a simple algorithm that tries to guess what most fitting suffix is for a given project name.

--- src/main/java/eme/generator/saving/NewProjectSavingStrategy.java
+++ src/main/java/eme/generator/saving/NewProjectSavingStrategy.java
@@ -27,7 +27,7 @@ public class NewProjectSavingStrategy extends AbstractSavingStrategy {
     @Override
     protected void beforeSaving(String projectName) {
         fileName = projectName;
-        IProject newProject = EMFProjectGenerator.createProject(projectName + "Model");
+        IProject newProject = EMFProjectGenerator.createProject(projectName + identifier(projectName));
         this.generatedProjectName = newProject.getName(); //
     }
 
@@ -46,6 +46,33 @@ public class NewProjectSavingStrategy extends AbstractSavingStrategy {
     @Override
     protected String fileName() {
         return fileName;
+    }
+
+    /**
+     * Returns the project identifier, which basically is a separator character and the String
+     * "Model" or "Model", depending on the naming type of the projects name.
+     * @param projectName is the projects name. determines which identifier is used.
+     * @return the project identifier.
+     */
+    private String identifier(String projectName) {
+        char[] candidates = { ' ', '.', '-', '_', ':' }; // possible separators
+        char separator = Character.MIN_VALUE; // no char (empty string) is default
+        int max = 0;
+        for (char candidate : candidates) { // for every candidate
+            int ctr = 0; // count occurrences in project name:
+            for (int i = 0; i < projectName.length(); i++) {
+                ctr = (candidate == projectName.charAt(i)) ? (ctr + 1) : ctr;
+            }
+            if (ctr > max) { // if candidate is new most used candidate
+                max = ctr; // set as new preferred separator
+                separator = candidate;
+            }
+        }
+        String suffix = "Model"; // default suffix
+        if (!projectName.matches(".*[A-Z].*")) { // if has no upper case
+            suffix = suffix.toLowerCase(); // use lower case suffix
+        }
+        return separator + suffix; // identifier = separator + suffix
     }
 
 }
