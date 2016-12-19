a8f9c7b12f6f583211d40f0523622a7116324fb5
Timur Saglam <uleay@student.kit.edu>
2016-12-19 11:41:40 +0100

    EDIT: Added a test case to the suite.

--- src/test/java/eme/AllHeadlessTests.java
+++ src/test/java/eme/AllHeadlessTests.java
@@ -6,12 +6,14 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import eme.generator.EObjectGeneratorTest;
 import eme.generator.EcoreMetamodelGeneratorTest;
+import eme.model.IntermediateModelTest;
 
 @RunWith(Suite.class)
 
 @SuiteClasses({
     EcoreMetamodelGeneratorTest.class,
-    EObjectGeneratorTest.class
+    EObjectGeneratorTest.class,
+    IntermediateModelTest.class
 })
 
 public class AllHeadlessTests {
