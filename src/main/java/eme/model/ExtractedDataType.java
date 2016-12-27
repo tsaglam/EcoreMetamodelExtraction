222bf229b75849e4311d657b66c57aad366c44bf
Timur Saglam <uleay@student.kit.edu>
2016-12-27 14:45:14 +0100

    ADD: Revisioned the ExtractedVariable System. It now uses a super class called ExtractedDataType

---
+++ src/main/java/eme/model/ExtractedDataType.java
@@ -0,0 +1,86 @@
+package eme.model;
+
+/**
+ * Represents a data type in the model.
+ * @author Timur Saglam
+ */
+public class ExtractedDataType {
+    private final String simpleName;
+    private final String fullName;
+    private int arrayDimension;
+    private int genericArguments;
+
+    /**
+     * Basic constructor, takes the full and the simple name.
+     * @param simpleName is the simple name of the data type, like "String", "List&ltint&gt" and
+     * "char[][]".
+     * @param fullName is the full name of the data type, like "java.lang.String", "java.util.list"
+     * and "char".
+     */
+    public ExtractedDataType(String simpleName, String fullName) {
+        this.simpleName = simpleName;
+        this.fullName = fullName;
+        if (simpleName.contains("<")) {
+            genericArguments = 1; // at least one
+        }
+        for (int i = 0; i < simpleName.length(); i++) {
+            if (simpleName.charAt(i) == ',') {
+                genericArguments++; // count additional generic arguments
+            }
+        }
+        arrayDimension = simpleName.split("\\[").length - 1; // count array dimensions
+    }
+
+    /**
+     * Getter for the array dimension.
+     * @return the array dimension, 0 if the type is not an array.
+     */
+    public int getArrayDimension() {
+        return arrayDimension;
+    }
+
+    /**
+     * getter for the full type name.
+     * @return the full type name.
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * getter for the number of generic arguments.
+     * @return the the number of generic arguments, 0 if the type is not generic.
+     */
+    public int getGenericArguments() {
+        return genericArguments;
+    }
+
+    /**
+     * getter for the simple type name.
+     * @return the simple type name.
+     */
+    public String getName() {
+        return simpleName;
+    }
+
+    /**
+     * Checks whether the data type is an array.
+     * @return true if it is an array.
+     */
+    public boolean isArray() {
+        return arrayDimension > 0;
+    }
+
+    /**
+     * Checks whether the data type is a generic type.
+     * @return true if it is generic.
+     */
+    public boolean isGeneric() {
+        return genericArguments > 0;
+    }
+
+    @Override
+    public String toString() {
+        return simpleName;
+    }
+}
--- src/main/java/eme/model/ExtractedVariable.java
+++ src/main/java/eme/model/ExtractedVariable.java
@@ -4,33 +4,32 @@ package eme.model;
  * Represents an extracted variable in the model. Can be a method parameter or an class variable.
  * @author Timur Saglam
  */
-public class ExtractedVariable {
-    private final String name;
-    private final String type;
+public class ExtractedVariable extends ExtractedDataType {
+    private final String identifier;
 
     /**
      * Basic constructor, creates variable.
      * @param name is the name of the variable.
-     * @param type is the type of the variable.
+     * @param simpleName is the simple name of the type of the variable, like "String",
+     * "List&ltint&gt" and "char[][]".
+     * @param fullName is the full name of type of the variable, like "java.lang.String",
+     * "java.util.list" and "char".
      */
-    public ExtractedVariable(String name, String type) {
-        this.name = name;
-        this.type = type;
+    public ExtractedVariable(String name, String simpleName, String fullName) {
+        super(simpleName, fullName);
+        this.identifier = name;
     }
 
     /**
-     * Getter for the name.
+     * Getter for the name of the variable.
      * @return the name.
      */
     public String getName() {
-        return name;
+        return identifier;
     }
 
-    /**
-     * getter for the type.
-     * @return the type.
-     */
-    public String getType() {
-        return type;
+    @Override
+    public String toString() {
+        return super.toString() + " " + identifier;
     }
 }
