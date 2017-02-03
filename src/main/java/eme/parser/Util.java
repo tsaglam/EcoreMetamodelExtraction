98a2ccbe1c87b622c0a2c84e644e9d7a5d94165e
Timur Saglam <uleay@student.kit.edu>
2017-02-03 14:41:41 +0100

    COMMENT: Added JDoc method comments.

--- src/main/java/eme/parser/Util.java
+++ src/main/java/eme/parser/Util.java
@@ -13,7 +13,7 @@ import eme.model.datatypes.WildcardStatus;
  * This class offers simple adapter methods for JDT functionality that makes code unreadable.
  * @author Timur Saglam
  */
-public final class Util { // TODO (HIGH) comments
+public final class Util {
 
     /**
      * Private constructor for static class.
@@ -22,6 +22,12 @@ public final class Util { // TODO (HIGH) comments
         // Private constructor.
     }
 
+    /**
+     * Determines the access level modifier of an {@link IMember} and returns it as {@link AccessLevelModifier}.
+     * @param iMember is the {@link IMember}.
+     * @return the {@link AccessLevelModifier}.
+     * @throws JavaModelException if there is a problem with the JDT API.
+     */
     public static AccessLevelModifier getModifier(IMember iMember) throws JavaModelException {
         int flags = iMember.getFlags();
         if (Flags.isPublic(flags)) {
@@ -34,10 +40,22 @@ public final class Util { // TODO (HIGH) comments
         return AccessLevelModifier.NO_MODIFIER;
     }
 
+    /**
+     * Returns the fully qualified name of an {@link IType}, including qualification for any containing types and
+     * packages. The character '.' is used as enclosing type separator.
+     * @param iType is the {@link IType}.
+     * @return the fully qualified name.
+     * @see eme.generator.saving.AbstractSavingStrategy#filePath()
+     */
     public static String getName(IType iType) {
         return iType.getFullyQualifiedName('.');
     }
 
+    /**
+     * Determines the {@link WildcardStatus} of an signature string.
+     * @param signature is the signature string.
+     * @return the {@link WildcardStatus}.
+     */
     public static WildcardStatus getWildcardStatus(String signature) {
         if (signature.contains(Character.toString(Signature.C_STAR))) {
             return WildcardStatus.WILDCARD; // is unbound wildcard
@@ -49,46 +67,106 @@ public final class Util { // TODO (HIGH) comments
         return WildcardStatus.NO_WILDCARD; // is no wildcard
     }
 
+    /**
+     * Checks if a type name contains generic arguments.
+     * @param name is the name of the type.
+     * @return true if it contains generic arguments.
+     */
     public static boolean hasGenericArguments(String name) {
         return name.contains(Character.toString(Signature.C_GENERIC_START));
     }
 
+    /**
+     * Checks if a signature contains a character constant indicating a lower bound wildcard type argument.
+     * @param signature is the signature string.
+     * @return true if it contains one.
+     */
     public static boolean hasLowerBound(String signature) {
         return signature.charAt(0) == Signature.C_SUPER;
     }
 
+    /**
+     * Checks if a signature contains a character constant indicating a upper bound wildcard type argument.
+     * @param signature is the signature string.
+     * @return true if it contains one.
+     */
     public static boolean hasUpperBound(String signature) {
         return signature.charAt(0) == Signature.C_EXTENDS;
     }
 
+    /**
+     * Checks if a {@link IMember} is abstract by checking its flags.
+     * @param iMember is the {@link IMember}.
+     * @return true if it is.
+     * @throws JavaModelException if there is a problem with the JDT API.
+     */
     public static boolean isAbstract(IMember iMember) throws JavaModelException {
         return Flags.isAbstract(iMember.getFlags());
     }
 
+    /**
+     * Checks if a {@link IMember} is an enum by checking its flags.
+     * @param iMember is the {@link IMember}.
+     * @return true if it is.
+     * @throws JavaModelException if there is a problem with the JDT API.
+     */
     public static boolean isEnum(IMember iMember) throws JavaModelException {
         return Flags.isEnum(iMember.getFlags());
     }
 
+    /**
+     * Checks if a {@link IMember} is final by checking its flags.
+     * @param iMember is the {@link IMember}.
+     * @return true if it is.
+     * @throws JavaModelException if there is a problem with the JDT API.
+     */
     public static boolean isFinal(IMember iMember) throws JavaModelException {
         return Flags.isFinal(iMember.getFlags());
     }
 
+    /**
+     * Checks if a type name is a nested type (OuterType.InnerType).
+     * @param name is the name of the type.
+     * @return true if it is a nested type.
+     */
     public static boolean isNestedType(String name) {
         return name.contains(".") && Character.isUpperCase(name.charAt(0));
     }
 
+    /**
+     * Checks if a {@link IMember} is static by checking its flags.
+     * @param iMember is the {@link IMember}.
+     * @return true if it is.
+     * @throws JavaModelException if there is a problem with the JDT API.
+     */
     public static boolean isStatic(IMember iMember) throws JavaModelException {
         return Flags.isStatic(iMember.getFlags());
     }
 
+    /**
+     * Checks if a signature contains a character constant indicating the start of an unresolved, named type in a
+     * signature.
+     * @param signature is the signature string.
+     * @return true if it contains one.
+     */
     public static boolean isUnresolved(String signature) {
         return signature.charAt(0) == Signature.C_UNRESOLVED;
     }
 
+    /**
+     * Checks if a signature is the string constant for the signature of result type void.
+     * @param signature is the signature string.
+     * @return true if it is void.
+     */
     public static boolean isVoid(String signature) {
         return Signature.SIG_VOID.equals(signature);
     }
 
+    /**
+     * Removes any generic arguments from a type name.
+     * @param name is the type name.
+     * @return the type name without generic arguments.
+     */
     public static String removeGenericArguments(String name) {
         return name.substring(0, name.lastIndexOf(Signature.C_GENERIC_START));
     }
