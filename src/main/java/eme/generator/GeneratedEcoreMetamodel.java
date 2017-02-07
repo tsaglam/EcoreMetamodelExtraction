88fb12a9332aeccb9cfc9845723ff3d215f04be5
Timur Saglam <uleay@student.kit.edu>
2017-02-07 16:36:18 +0100

    ADD: Container class for generated Ecore metamodels.

---
+++ src/main/java/eme/generator/GeneratedEcoreMetamodel.java
@@ -0,0 +1,55 @@
+package eme.generator;
+
+import org.eclipse.emf.ecore.EPackage;
+
+import eme.generator.saving.SavingInformation;
+
+/**
+ * This is a container class for generated Ecore metamodels.
+ * @author Timur Saglam
+ */
+public class GeneratedEcoreMetamodel {
+    private final EPackage root;
+    private SavingInformation savingInformation;
+
+    /**
+     * Basic constructor.
+     * @param root is the root {@link EPackage} of the generated Ecore metamodel.
+     */
+    public GeneratedEcoreMetamodel(EPackage root) {
+        this.root = root;
+    }
+
+    /**
+     * Accessor for the root {@link EPackage}.
+     * @return the root.
+     */
+    public EPackage getRoot() {
+        return root;
+    }
+
+    /**
+     * Accessor for the {@link SavingInformation} object.
+     * @return the savingInformation
+     */
+    public SavingInformation getSavingInformation() {
+        return savingInformation;
+    }
+
+    /**
+     * Checks whether this metamodel was already saved. This means is contains saving informations (a
+     * {@link SavingInformation} object).
+     * @return true if it was.
+     */
+    public boolean isSaved() {
+        return savingInformation != null;
+    }
+
+    /**
+     * Mutator for the {@link SavingInformation} object.
+     * @param savingInformation the savingInformation to set
+     */
+    public void setSavingInformation(SavingInformation savingInformation) {
+        this.savingInformation = savingInformation;
+    }
+}
