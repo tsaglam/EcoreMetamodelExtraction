497e0fcf8272fddb12a5674d120675351afb3e2e
Timur Saglam <uleay@student.kit.edu>
2018-02-01 15:45:05 +0100

    FIX: Fixed logging, don't even ask me why it was broken again.

--- src/main/java/eme/handlers/MainHandler.java
+++ src/main/java/eme/handlers/MainHandler.java
@@ -1,5 +1,6 @@
 package eme.handlers;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -17,6 +18,7 @@ public abstract class MainHandler extends AbstractHandler {
     private static final Logger logger = LogManager.getLogger(MainHandler.class.getName());
 
     static { // Set logging level.
+        BasicConfigurator.configure();
         Logger rootLogger = Logger.getRootLogger();
         rootLogger.setLevel(Level.INFO);
     }
